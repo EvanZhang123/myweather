2853976428068188f6533733bbfedfffd945945a
Evan <494450102@qq.com>
2016-06-22 17:35:27 +0800

    新增切换城市和手动更新天气的功能。

--- src/com/myweather/app/activity/WeatherActivity.java
+++ src/com/myweather/app/activity/WeatherActivity.java
@@ -13,11 +13,13 @@ import android.preference.PreferenceManager;
 import android.test.PerformanceTestCase;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class WeatherActivity extends Activity{
+public class WeatherActivity extends Activity implements OnClickListener{
 	private LinearLayout weatherInfoLayout;
 	/**
 	 * 用于显示城市名,发布时间，天气描述信息
@@ -32,6 +34,12 @@ public class WeatherActivity extends Activity{
 	private TextView temp1Text;
 	private TextView temp2Text;
 	private TextView currentDateText;
+	
+	/**
+	 * 切换城市按钮及更换天气按钮
+	 */
+	private Button switchCity;
+	private Button refreshWeather;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +65,33 @@ public class WeatherActivity extends Activity{
 		}else{
 			//没有县级代号就直接显示本地天气
 			showWeather();
+		}
+		
+		switchCity = (Button)findViewById(R.id.swich_city);
+		refreshWeather = (Button)findViewById(R.id.refresh_weather);
+		switchCity.setOnClickListener(this);
+		refreshWeather.setOnClickListener(this);
+	}
+	
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.swich_city:
+			Intent intent = new Intent(this, ChooseAreaActivity.class);
+			intent.putExtra("from_weather_activity", true);
+			startActivity(intent);
+			finish();
+			break;
+		case R.id.refresh_weather:
+			publishText.setText("同步中...");
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+			String weatherCode = prefs.getString("weather_code", "");
+			if(!TextUtils.isEmpty(weatherCode)){
+				queryWeatherInfo(weatherCode);
+			}
+			break;
+		default:
+			break;
 		}
 	}
 
@@ -125,12 +160,14 @@ public class WeatherActivity extends Activity{
 	private void showWeather(){
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		cityNameText.setText(prefs.getString("city_name", ""));
-		temp1Text.setText(prefs.getString("temp2", ""));
-		temp2Text.setText(prefs.getString("temp1", ""));
+		temp1Text.setText(prefs.getString("temp1", ""));
+		temp2Text.setText(prefs.getString("temp2", ""));
 		weatherDespText.setText(prefs.getString("weather_desp", ""));
 		publishText.setText("今天" + prefs.getString("publish_time", "") + "发布");
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
 	}
+
+	
 }
